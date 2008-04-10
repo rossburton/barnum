eeba7de0bae2b1956f7f225e4b076830d2d0126d
Ross Burton <ross.burton@intel.com>
2008-04-10 14:56:22 +0100

    Add a tool to create vCards

---
+++ gen_vcard.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+
+import sys, random
+import gen_data
+
+try:
+    count = int (sys.argv[1])
+except:
+    count = 1
+
+vcard = """BEGIN:VCARD
+VERSION:3.0
+FN:%s
+N:%s;%s;;;
+BDAY:%s
+EMAIL;TYPE=WORK:%s
+X-JABBER;TYPE=HOME:%s
+ADR;TYPE=HOME:;;%s;%s;%s;%s;USA
+END:VCARD"""
+
+for i in range (0, count):
+    name = gen_data.create_name()
+    adr = gen_data.create_city_state_zip()
+
+    print >>open(str(i), "w"), vcard % (' '.join(name),
+                   name[1], name[0],
+                   gen_data.create_birthday(),
+                   gen_data.create_email(name=name),
+                   gen_data.create_email(name=name),
+                   gen_data.create_street(),
+                   adr[1], adr[2], adr[0],
+                   )
