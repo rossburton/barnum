f876deeb6c43f8dffa35a8696dfcba91f3f0e7f8
Ross Burton <ross.burton@intel.com>
2009-01-09 19:41:59 +0000

    Add company to the vcard (thanks Raphael Wimmer)

--- gen_vcard.py
+++ gen_vcard.py
@@ -12,6 +12,7 @@ vcard = """BEGIN:VCARD
 VERSION:3.0
 FN:%s
 N:%s;%s;;;
+ORG:%s
 BDAY:%s
 EMAIL;TYPE=WORK:%s
 X-JABBER;TYPE=HOME:%s
@@ -21,9 +22,11 @@ END:VCARD"""
 for i in range (0, count):
     name = gen_data.create_name()
     adr = gen_data.create_city_state_zip()
+    company = gen_data.create_company_name()
 
     print >>open(str(i), "w"), vcard % (' '.join(name),
                    name[1], name[0],
+                   company,
                    gen_data.create_birthday(),
                    gen_data.create_email(name=name),
                    gen_data.create_email(name=name),
